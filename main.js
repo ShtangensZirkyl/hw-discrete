c9746479811b3d72a51d9ee702418eb9a7c4412d
Maxim <shtamm_m@mail.ru>
2021-12-13 21:11:50 +0300

    Added pressure, mass center

--- main.js
+++ main.js
@@ -6,18 +6,16 @@ function main() {
 
 	var	N       = 8;
 
-	let t		= 0;     //время
-		dt 		= 0.01;  //переменная интегрироваания
-		k		= 0.1;   //коэф. жесткости
-		b 		= 0.1;
+	let	dt 		= 0.01;  //переменная интегрироваания
+	let	k		= 0.1;   //коэф. жесткости
 	
 	let v_y 	= [];
 	let v_x 	= [];
 	let	F_x 	= [];
 	let	F_y 	= [];
 
-		phi     = 2 * Math.PI / N;
-		Radius  = 4;
+	let	phi     = 2 * Math.PI / N;
+	let	Radius  = 4;
 	let	x       = [];
 	let	y     	= [];
 
@@ -29,10 +27,19 @@ function main() {
 	}
 
 	//v_x[1] = -1;
+	let l = Math.sqrt(Math.pow((x[1] - x[0]),2) + Math.pow((y[1] - y[0]),2));
 
-	let l 		= Math.sqrt(Math.pow((x[1] - x[0]),2) + Math.pow((y[1] - y[0]),2));
-	let l2 		= Math.sqrt(Math.pow((x[1] - x[2]),2) + Math.pow((y[1] - y[2]),2));
-	let l3 		= Math.sqrt(Math.pow((x[2] - x[0]),2) + Math.pow((y[2] - y[0]),2));
+	/*function Length(){
+		for (let i = 0; i < N-1; i++) {
+			l0[i] 		= Math.sqrt(Math.pow((x[i] - x[i+1]),2) + Math.pow((y[i] - y[i+1]),2));
+		}
+
+		l0[N-1] = Math.sqrt(Math.pow((x[N-1] - x[0]),2) + Math.pow((y[N-1] - y[0]),2));
+	}
+
+	Length();
+
+	console.log(l0);*/
 	
 	function Elastic_forces(k1, k2, k3) {
 		var	dl_right;
@@ -65,17 +72,9 @@ function main() {
 		F_r_y 	= -k * (dl_right - l) * cos_r;
 		F_r_x 	= -k * (dl_right - l) * sin_r;
 
-		F_y[k2] 	= F_r_y - F_l_y;
-		F_x[k2] 	= F_r_x - F_l_x;
-
-		//console.log("l2 = " + l2 + ", l3 = " + l3 + ", l = " + l + ", i = " + i + ", dl_left-l = " +(dl_left-l) + ", dl_right-l = " +(dl_right-l) );
-
-		//console.log("i = " + i + ", f_x = " + F_x[i] + ", f_y = " + F_y[i] + ", dl_left = " + (dl_left-l) );
-
-		v_y[k2] 	+= F_y[k2] * dt;
-		y[k2] 		+= v_y[k2] * dt;
-		v_x[k2]  	+= F_x[k2] * dt;
-		x[k2] 		+= v_x[k2] * dt;
+		//console.log("i = " + k2 + ", dl_left - l = " + (dl_left - l) + ", dl_right - l= " + (dl_right - l));
+		console.log("i = " + k2 + ", f_l_x = " + F_l_x + ", f_l_y = " + F_l_y + ", f_r_x = " + F_r_x + ", f_r_y = " + F_r_y );
+		return [F_l_x, F_l_y, F_r_x, F_r_y];
 	}
 	
 	function Collision() {
@@ -101,19 +100,63 @@ function main() {
 		return 1/2 * Math.abs(S1 + x[N-1] * y[1] - S2 + x[1] * y[N-1]);
 	}
 
+	function Sum(x, lower_bound, upper_bound) {
+		let s = 0;
+		for (let i = lower_bound; i < upper_bound; i++) {
+			s += x[i];
+		}
+		return s;
+	}
 
+	function mass_center() {
+		let x_mass_center = Sum(x, 0, N) / N;
+		let y_mass_center = Sum(y, 0, N) / N;
+		return [x_mass_center, y_mass_center];
+	}
+
+	let s0 		= Square();
+
+	let F_pres_x = [];
+	let	F_pres_y = [];
+
+	function Pressure() {
+		let k_pres = 1;
+		let sin_p;
+		let cos_p;
+		let rad;
+		let x_c = mass_center()[0];
+		let y_c = mass_center()[1];
+
+		for (let i = 0; i < N; i++) {
+			rad = Math.sqrt(Math.pow((x[i] - x_c),2) + Math.pow((y[i] - y_c),2));
+			cos_p = (x[i] - x_c) / rad;
+			sin_p = (y[i] - y_c) / rad;
+
+			F_pres_x[i] = k_pres * (1 - Square() / s0) * cos_p;
+			F_pres_y[i] = k_pres * (1 - Square() / s0) * sin_p;
+		}
+	}
 
 	function physics() {
+		let F_elast = [];
 
-		Elastic_forces(N-1, 0, 1);
+		F_elast[0] = Elastic_forces(N-1, 0, 1);
 		for (let i = 1; i < N - 1; i++) {
-			Elastic_forces(i-1, i, i+1);
+			F_elast[i] = Elastic_forces(i-1, i, i+1);
 		}
-		Elastic_forces(N - 2, N - 1, 0);
+		F_elast[N-1] = Elastic_forces(N - 2, N - 1, 0);
 
-		console.log(Square());
+		for (let i = 0; i < N; i++) {
+			F_y[i] = F_elast[i][3] + F_elast[i][1];
+			F_x[i] = F_elast[i][0] + F_elast[i][2];
 
-		for (i = 0; i < N; i++) {
+			v_y[i] += F_y[i] * dt;
+			y[i] += v_y[i] * dt;
+			v_x[i] += F_x[i] * dt;
+			x[i] += v_x[i] * dt;
+		}
+
+		for (let i = 0; i < N; i++) {
 			if (x[i]>10 || x[i]<-10) {
 				v_x[i] = -v_x[i];
 			}
@@ -122,7 +165,6 @@ function main() {
 				v_y[i] = -v_y[i];
 			}
 		}
-		//Collision();
 	}
 
 	function draw() {
@@ -144,7 +186,7 @@ function main() {
 		for (let i = 0; i < N; i++) {
 			ctx.beginPath();
 			ctx.fillStyle = 'black';
-			ctx.arc(200 + x[i] * 20, 200 + y[i] * 20, 4, 0, 2*Math.PI);
+			ctx.arc(200 + x[i] * 20, 200 + y[i] * 20, 4+i, 0, 2*Math.PI);
 			ctx.fill();
 		}
 	}
